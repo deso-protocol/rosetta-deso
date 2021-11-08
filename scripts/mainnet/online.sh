18ea834adab37f56755c5c999a3d33ec3c1a7966
diamondhands <diamondhands@bitcloutdev.com>
2021-11-07 21:59:31 -0800

    Fix default storage directory in mainnet Rosetta script

--- scripts/mainnet/online.sh
+++ scripts/mainnet/online.sh
@@ -1,5 +1,5 @@
 (cd ../../ && go build -o rosetta-deso -gcflags="all=-N -l" main.go && ./rosetta-deso run \
   --mode ONLINE \
   --port 17005 \
-  --data-directory /media/storage1/rosetta-online-mainnet-00000 \
+  --data-directory /tmp/rosetta-online-mainnet-00000 \
 )
