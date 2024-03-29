9083f3dbc40402722c322867c093312d85a4d9c8
Lazy Nina <lazynina84@gmail.com>
2024-03-29 15:46:27 -0400

    Remove relic tags in scripts

--- scripts/testnet/online.sh
+++ scripts/testnet/online.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -v
 
-(cd ../../ && go build -o rosetta-deso -gcflags="all=-N -l" -tags=relic main.go && ./rosetta-deso run \
+(cd ../../ && go build -o rosetta-deso -gcflags="all=-N -l" main.go && ./rosetta-deso run \
   --glog-v=0 \
   --glog-vmodule="*bitcoin_manager*=0,*balance*=0,*view*=0,*frontend*=0,*peer*=0,*addr*=0,*network*=0,*utils*=0,*connection*=0,*main*=0,*server*=0,*mempool*=0,*miner*=0,*blockchain*=0" \
   --mode ONLINE \
--- scripts/testnet/regtest.sh
+++ scripts/testnet/regtest.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -v
 
-(cd ../../ && go build -o rosetta-deso -gcflags="all=-N -l" -tags=relic main.go && ./rosetta-deso run \
+(cd ../../ && go build -o rosetta-deso -gcflags="all=-N -l" main.go && ./rosetta-deso run \
   --mode ONLINE \
   --port 17005 \
   --data-directory /tmp/rosetta-regtest-online-2023-04-09-00012 \
